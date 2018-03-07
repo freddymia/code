f21cd40d0213e59c7d5449516c22da13c1cb9bcc
finga <freddy.ingaec@gmail.com>
2018-03-07 11:47:44 -0500

    PlayWithNumber created!

---
+++ codemonk/src/main/java/ec/codemonk/basicsOfInputOutput/PlayWithNumbers.java
@@ -0,0 +1,25 @@
+package ec.codemonk.basicsOfInputOutput;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PlayWithNumbers {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int Q = Integer.parseInt(br.readLine());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
