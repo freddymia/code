a0250c9a6e6b8b0a0bb04f70fe450345f82a4e1e
finga <freddy.ingaec@gmail.com>
2018-04-03 10:00:07 -0500

    V1

--- codemonk/src/main/java/ec/codemonk/basics/bit/manipulation/SubhamAndBinaryStrings.java
+++ codemonk/src/main/java/ec/codemonk/basics/bit/manipulation/SubhamAndBinaryStrings.java
@@ -5,10 +5,31 @@ import java.io.InputStreamReader;
 
 public class SubhamAndBinaryStrings {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int numberTest = Integer.parseInt(br.readLine());
+
+        while (numberTest-- > 0) {
+            int sizeStr = Integer.parseInt(br.readLine());
+            String str = br.readLine();
+
+            moving(str, sizeStr);
+
+        }
+    }
+
+    static void moving(String str, int sizeStr) {
+        String sn = "";
+        for (int i = 1; i < sizeStr; i++) {
+            sn = str.substring(i, sizeStr) + str.substring(0, i);
+            System.out.println(sn);
+        }
+    }
+    
+    static validateDecimal(String binari) {
+        
     }
 
 }
